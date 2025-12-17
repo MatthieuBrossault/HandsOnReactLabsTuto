7e403591f77916e21a9bb3233b43e4932833a93e
Matthieu Brossault <m.brossault@elasoftware.fr>
2025-12-17 15:59:46 +0100

    add project api + display loading, errors and pagging to projects page

--- src/projects/ProjectPage.tsx
+++ src/projects/ProjectPage.tsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects"
+import { useState, useEffect } from "react";
 import type { Project } from "./Project";
 import ProjectList from "./ProjectList"
+import projectAPI from "./projectAPI";
 
 function ProjectPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(page);
+        setError('');
+        if (page === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+      }
+      catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, [page]);
+
+  const handleMoreClick = () => {
+    setPage((currentPage) => currentPage + 1);
+  };
 
   const saveProject = (project: Project) => {
     let updatedProjects = projects.map((p: Project) => {
@@ -16,7 +46,36 @@ function ProjectPage() {
   return (
     <>
       <h1>Projects</h1>
+      {error && error.length > 0 && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse " />
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
       <ProjectList projects={projects} onSave={saveProject} />
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary" />
+          <p>Loading...</p>
+        </div>
+      )}
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   )
 }
